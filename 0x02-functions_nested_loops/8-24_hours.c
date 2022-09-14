6d9875d1fba9b1b811aed0241d68f06a0bd20868
Paulelo <pauladeyemi591@gmail.com>
2022-09-14 12:04:43 -0700

    Printing time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main- prints time
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int hrs = 0;
+	int min = 0;
+	int hrs_remain;
+	int min_remain;
+
+	while (hrs <= 23)
+	{
+	while (min <= 59)
+	{
+	min_remain = min % 10;
+	hrs_remain = hrs % 10;
+	_putchar(hrs / 10 + '0');
+	_putchar(hrs_remain + '0');
+	_putchar(':');
+	_putchar(min / 10 + '0');
+	_putchar(min_remain + '0');
+	min++;
+	_putchar('\n');
+	}
+	hrs++;
+	min = 0;
+}
+}
