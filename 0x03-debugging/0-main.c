12568f65a49c47a6f1ecc33169986c484e2fec65
Paulelo <pauladeyemi591@gmail.com>
2022-09-15 02:41:51 -0700

    Test file 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main- test function that prints if int is nagative or positive
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This directory contains test files for C codes written earlier in this repository.
