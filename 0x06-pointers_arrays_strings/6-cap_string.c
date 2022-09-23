d10469bec458c4a3402fb36b9afbf6c570defb5f
Paulelo <pauladeyemi591@gmail.com>
2022-09-22 21:42:50 -0700

    Initializing alpha case conversion

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* * cap_string - Capitalizes all inputs
+* @l: is variable holding string to be capitalized
+* Return: x
+*/
+char *cap_string(char *l)
+{
+	char car[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	len = 13;
+	int a = 0, i;
+
+	while (l[i])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || l[a - 1] == car[i]) && (l[a] >= 97 && l[a] >= 122))
+				l[a] = l[a] - 32;
+			i++;
+		}
+		a++;
+	}
+	return (x);
+}
