900cee54053275e710d83b194147ff9a2c986575
Paulelo <pauladeyemi591@gmail.com>
2022-09-24 06:29:57 -0700

    Hot fix on 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 
 /**
  * cap_string - Capitalizes all words of a str
@@ -8,28 +8,28 @@
  */
 char *cap_string(char *str)
 {
-        int index = 0; 
+	int index = 0;
 
-        while (str[index])
-        {
-                while (!(str[index] >= 'a' && str[index] <= 'z'))
-                        index++;
-                if (str[index - 1] == ' ' ||
-                    str[index - 1] == '\t' ||
-                    str[index - 1] == '\n' ||
-                    str[index - 1] == ',' ||
-                    str[index - 1] == ';' ||
-                    str[index - 1] == '.' ||
-                    str[index - 1] == '!' ||
-                    str[index - 1] == '?' ||
-                    str[index - 1] == '"' ||
-                    str[index - 1] == '(' ||
-                    str[index - 1] == ')' ||
-                    str[index - 1] == '{' ||
-                    str[index - 1] == '}' ||
-                    index == 0)
-                        str[index] -= 32;
-                index++;
-        }
-        return (str);
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+			if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+				str[index] -= 32;
+			index++;
+	}
+	return (str);
 }
