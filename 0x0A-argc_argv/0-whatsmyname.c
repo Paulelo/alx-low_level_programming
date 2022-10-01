dcb46bf329a4bbd64eb7da2324c51c6c6e1fc3bd
Paulelo <pauladeyemi591@gmail.com>
2022-09-30 20:13:28 -0700

    Hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,10 @@
 /**
 * main - prints the programe name
 * @argc: is variable tgat holds the number of argument passed
-* @argv[]: is an array containing the argument
+* @argv: is an array containing the argument
 * Return: on success 0
 */
-int main(int foo (__attribute__((unused)) int argc), char* argv[])
+int main(int argc __attribute__((unused)), char* argv[])
 {
 	printf("%s\n", argv[0]);
 
