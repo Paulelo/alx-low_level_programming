f9a45701ba7153c922b2201d1117c95f8517332e
Paulelo <pauladeyemi591@gmail.com>
2022-10-02 17:40:23 -0700

    Intiating argument add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - adds all positive int argument passed to the programme
+* @argc: counts all arguments passed to the programme
+* @argv: is an arry of char that holds value of all arguments passed to programme
+* Return: onsuccess 0
+*/
+int main(int argc, char *argv[])
+{
+	int result = 0, num, i, j, k;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		}
+	}
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
