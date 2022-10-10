474e38ab16c1930314d55ba784221c6af84885dd
Paulelo <pauladeyemi591@gmail.com>
2022-10-10 06:38:20 -0700

    Using a predefined macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints outs the name of file this programme is compiled from
+* Return: 0 on success
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
