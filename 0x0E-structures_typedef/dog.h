d82f07ba665f480c55e646da946faa696a32f33e
Paulelo <pauladeyemi591@gmail.com>
2022-10-13 10:25:47 -0700

    Declearing a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - is dog's informations
+* @name: first information
+* @age: second information
+* @owner: third information
+*
+* Description: the structure decleared below is
+* one that computes the information of a dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
