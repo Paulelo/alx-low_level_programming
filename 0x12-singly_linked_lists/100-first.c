969f0d8407bfcd8846532ebb3fb66ce14724440a
Paulelo <pauladeyemi591@gmail.com>
2022-10-20 21:03:45 -0700

    Printing string

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void __attribute__((constructor)) here(void);
+
+/**
+* here - prints a string before tge main function is encountered
+*/
+void here(void)
+{
+	printf("You're beat! and yet, you must allow,"
+		"\nI bore my house upon my back!\n");
+}
